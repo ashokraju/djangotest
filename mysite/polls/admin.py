db7f2ab540089478b13e1ad03570ea6c5dbb0228
Ashok Raju <araju75@gmail.com>
2012-05-17 00:20:10 -0400

    poll inlined with choices

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,4 +1,16 @@
 from polls.models import Poll
+from polls.models import Choice
 from django.contrib import admin
 
-admin.site.register(Poll)
+class ChoiceInline(admin.StackedInline):
+  model = Choice
+  extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+  fieldsets = [
+      (None,{'fields':['question']}),
+      ('Date Information', {'fields':['pub_date'], 'classes': ['collapse']})
+  ]
+  inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
