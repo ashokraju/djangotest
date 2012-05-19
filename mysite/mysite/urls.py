0f620255b7f9b3d73029895e3ed8decc6068e603
Black Tiger <araju75@gmail.com>
2012-05-19 15:22:26 -0400

    done tutorial! BITCHASS

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,18 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^polls/$', 'polls.views.index'),
-    (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    (r'^polls/(?P<poll_id>\d+)/results/$',
-        'polls.views.results'),
-    (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^mysite/', include('mysite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,32 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from polls.models import Choice, Poll
 
 def index(request):
-  return HttpResponse("Hello, world. You're at poll index.")
+  latest_polls = Poll.objects.all().order_by('-pub_date')[:5]
+  return render_to_response('polls/index.html', {'latest_poll_list':latest_polls})
 
 def detail(request, poll_id):
-  return HttpResponse("You're looking at poll %s." % poll_id)
+  p = get_object_or_404(Poll, pk=poll_id)
+  return render_to_response('polls/detail.html', {'poll': p},
+      context_instance=RequestContext(request))
 
 def results(request, poll_id):
   return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
-  return HttpResponse("You're voting on poll %s." % poll_id)
+  p = get_object_or_404(Poll, pk=poll_id)
+  try:
+    selected_choice = p.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    # Re-display the poll voting form.
+    return render_to_response('polls/detail.html', {
+        'poll': p,
+        'error_message': "You didn't select a valid choice."
+        }, context_instance=RequestContext(request))
+  else:
+    selected_choice.votes +=1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
